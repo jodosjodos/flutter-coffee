659ebba027694059005c36ea1112bdde3e747f09
jodosjodos <jeandedieu2030@gmail.com>
2024-04-24 07:51:38 +0200

    [add] done with Columns

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,41 @@
 import 'package:coffee_card/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MaterialApp(home: Home()));
 }
 
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:
+          AppBar(title: const Text("Sandbox"), backgroundColor: Colors.grey),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            width: 100,
+            color: Colors.red,
+            child: const Text("one"),
+          ),
+          Container(
+            width: 200,
+            color: Colors.green,
+            child: const Text("two"),
+          ),
+          Container(
+            width: 300,
+            color: Colors.blue,
+            child: const Text("three"),
+          )
+        ],
+      ),
+    );
+  }
+}
 // containers 
